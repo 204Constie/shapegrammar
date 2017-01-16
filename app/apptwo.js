b8f747dff5cc1d91fd2a8d5a3e5c41c049556c87
204Constie <kat.bana204@gmail.com>
2017-01-16 21:02:47 +0100

    add beauty check

--- app/apptwo.js
+++ app/apptwo.js
@@ -4,7 +4,7 @@
 var x = 30;
 var y = 30;
 var r = 15;
-var iter = 0;
+//var s = 1;
 
 var shapesData = [{'x': x, 'y': y, 'r': r, 'color': '#fff176'}];
 var colorData = ['#ffeb3b', '#fffde7', '#fff9c4', '#fff59d', '#fff176', '#ffee58', '#ffeb3b',
@@ -87,79 +87,6 @@ function genRadius(e){
 	}
 };
 
-
-function swap(array, firstID, secondID){
-			var tmp = array[firstID];
-			array[firstID] = array[secondID];
-			array[secondID] = tmp;
-		};
-
-function indexOfMin(array, startID){
-			var minValue = array[startID];
-			var minID = startID;
-
-			for(var i=minID+1; i<array.length; i++){
-				if(array[i]<minValue){
-					minID = i;
-					minValue = array[i];
-				}
-			}
-			return minID;
-		};
-
-
-function realSort(array){
-				var tmpID;
-				for(var j=0; j<array.length; j++){
-					tmpID = indexOfMin(array, j);
-					swap(array, tmpID, j);
-				}
-				return array;
-			};
-
-
-
-function sortElements(array){
-	var tmp = [];
-	var bigTmp = [];
-
-	for(var j=0; j<array.length; j++){
-		tmp.push(array[j].r);
-	}
-
-	tmp = realSort(tmp);
-	for(var k=0; k<tmp.length; k++){
-		for(var z=0; z<array.length; z++){
-			if(tmp[k] === array[z].r){
-				console.log('pipa');
-				bigTmp.push(array[z]);
-			}
-		}
-	}
-
-	return bigTmp;
-};
-
-function beautyCheck(){
-	var tmp = [];
-	for(var i=shapesData.length-1; i>shapesData.length - 10; i--){
-		tmp.push(shapesData[i]);
-	}
-
-	console.log('tmp: ', tmp);
-	tmp = sortElements(tmp);
-	console.log('jcsja: ', tmp.splice(4, 5));
-
-	tmp.splice(4, 5);
-
-	shapesData.splice(shapesData.length - 11, 10);
-	for(var p=0; p<tmp.length; p++){
-		shapesData.push(tmp[p]);
-	}
-	console.log('shapesData: ', shapesData);
-	update();
-};
-
 function clickEvent(e){
 
 	var randColor = Math.floor((Math.random() * 13) + 1) - 1;
@@ -169,13 +96,7 @@ function clickEvent(e){
 
 	if(status){
 		shapesData.push({'x': x, 'y': y, 'r': r, 'color': colorData[randColor]});
-		if(iter !== 10){
-			iter++;
-			update();
-		} else {
-			beautyCheck();
-			iter = 0;
-		}
+		update();
 	} else {
 		update();
 	}
@@ -190,6 +111,7 @@ function update(){
 	var shapeAttributes = shapes.attr('cx', function(d){ return d.x; })
 								.attr('cy', function(d){ return d.y; })
 								.attr('r', function(d){ return d.r; })
+								//.attr('transform', function(d){ return 'scale(' + d.s + ')'; })
 								.attr('fill', function(d){ return d.color; })
 								.on('click', clickEvent);
 };
